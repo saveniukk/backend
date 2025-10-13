99aa78f4715ec0dfbf784f6a60cf5aad5cff7d4d
Myroslava Saveniuk <saveniukf@gmail.com>
2025-10-13 22:58:06 +0300

    add AppService

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,10 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
-  }
-}
+  checkHealth() {
+    return {
+      status: 'ok',
+      date: new Date().toISOString(),
+    };
+  };
+};
