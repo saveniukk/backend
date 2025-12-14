94033db1d360811f7f0867c1ea68a947b4d417d7
Myroslava Saveniuk <saveniukf@gmail.com>
2025-12-14 15:34:27 +0200

    feat: add interfaces for entities

---
+++ src/types.ts
@@ -0,0 +1,17 @@
+export interface User {
+  id: string;
+  name: string;
+}
+
+export interface Category {
+  id: string;
+  name: string;
+}
+
+export interface Record {
+  id: string;
+  userId: string;
+  categoryId: string;
+  timestamp: string;
+  amount: number;
+}
